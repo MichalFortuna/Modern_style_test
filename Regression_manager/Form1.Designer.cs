aa803bfad1f0884539e2679e212e3899ab02de0d
Michał <Micha@LAPTOP-03P9QUND>
2018-02-19 21:51:23 +0100

    Add project files.

---
+++ Regression_manager/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Regression_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5_exit2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1_exit = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3_info = new System.Windows.Forms.PictureBox();
+            this.pictureBox4_user = new System.Windows.Forms.PictureBox();
+            this.Left_Menu_Button = new System.Windows.Forms.Button();
+            this.pictureBox2_options = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5_exit2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_exit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3_info)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4_user)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_options)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.panel2.Controls.Add(this.pictureBox6);
+            this.panel2.Controls.Add(this.pictureBox5_exit2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1582, 58);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(592, 8);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(439, 42);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5_exit2
+            // 
+            this.pictureBox5_exit2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5_exit2.Image")));
+            this.pictureBox5_exit2.Location = new System.Drawing.Point(1518, 12);
+            this.pictureBox5_exit2.Name = "pictureBox5_exit2";
+            this.pictureBox5_exit2.Size = new System.Drawing.Size(37, 34);
+            this.pictureBox5_exit2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5_exit2.TabIndex = 8;
+            this.pictureBox5_exit2.TabStop = false;
+            this.pictureBox5_exit2.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5_exit2.MouseLeave += new System.EventHandler(this.pictureBox5_MouseLeave);
+            this.pictureBox5_exit2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(72)))));
+            this.label1.Location = new System.Drawing.Point(72, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SETTINGS";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.pictureBox1_exit);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox3_info);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox4_user);
+            this.panel1.Controls.Add(this.Left_Menu_Button);
+            this.panel1.Controls.Add(this.pictureBox2_options);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 58);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(60, 795);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(51, 23);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(172, 44);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox7.TabIndex = 1;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(72)))));
+            this.label4.Location = new System.Drawing.Point(72, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "EXIT";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1_exit
+            // 
+            this.pictureBox1_exit.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1_exit.Image")));
+            this.pictureBox1_exit.Location = new System.Drawing.Point(9, 297);
+            this.pictureBox1_exit.Name = "pictureBox1_exit";
+            this.pictureBox1_exit.Size = new System.Drawing.Size(37, 34);
+            this.pictureBox1_exit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1_exit.TabIndex = 7;
+            this.pictureBox1_exit.TabStop = false;
+            this.pictureBox1_exit.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1_exit.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1_exit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(72)))));
+            this.label3.Location = new System.Drawing.Point(72, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "INFO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(69)))), ((int)(((byte)(72)))));
+            this.label2.Location = new System.Drawing.Point(72, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "USERS";
+            // 
+            // pictureBox3_info
+            // 
+            this.pictureBox3_info.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3_info.Image")));
+            this.pictureBox3_info.Location = new System.Drawing.Point(9, 231);
+            this.pictureBox3_info.Name = "pictureBox3_info";
+            this.pictureBox3_info.Size = new System.Drawing.Size(37, 34);
+            this.pictureBox3_info.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3_info.TabIndex = 3;
+            this.pictureBox3_info.TabStop = false;
+            this.pictureBox3_info.MouseLeave += new System.EventHandler(this.pictureBox3_MouseLeave);
+            this.pictureBox3_info.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox3_MouseMove);
+            // 
+            // pictureBox4_user
+            // 
+            this.pictureBox4_user.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4_user.Image")));
+            this.pictureBox4_user.Location = new System.Drawing.Point(9, 169);
+            this.pictureBox4_user.Name = "pictureBox4_user";
+            this.pictureBox4_user.Size = new System.Drawing.Size(37, 34);
+            this.pictureBox4_user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4_user.TabIndex = 4;
+            this.pictureBox4_user.TabStop = false;
+            this.pictureBox4_user.MouseLeave += new System.EventHandler(this.pictureBox4_MouseLeave);
+            this.pictureBox4_user.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseMove);
+            // 
+            // Left_Menu_Button
+            // 
+            this.Left_Menu_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Left_Menu_Button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.Left_Menu_Button.FlatAppearance.BorderSize = 0;
+            this.Left_Menu_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Left_Menu_Button.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Left_Menu_Button.Image = ((System.Drawing.Image)(resources.GetObject("Left_Menu_Button.Image")));
+            this.Left_Menu_Button.Location = new System.Drawing.Point(9, 21);
+            this.Left_Menu_Button.Name = "Left_Menu_Button";
+            this.Left_Menu_Button.Size = new System.Drawing.Size(37, 43);
+            this.Left_Menu_Button.TabIndex = 3;
+            this.Left_Menu_Button.UseVisualStyleBackColor = true;
+            this.Left_Menu_Button.Click += new System.EventHandler(this.Left_Menu_Button_Click);
+            // 
+            // pictureBox2_options
+            // 
+            this.pictureBox2_options.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2_options.Image")));
+            this.pictureBox2_options.Location = new System.Drawing.Point(9, 111);
+            this.pictureBox2_options.Name = "pictureBox2_options";
+            this.pictureBox2_options.Size = new System.Drawing.Size(37, 34);
+            this.pictureBox2_options.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2_options.TabIndex = 2;
+            this.pictureBox2_options.TabStop = false;
+            this.pictureBox2_options.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            this.pictureBox2_options.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(60, 58);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1522, 795);
+            this.panel3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(36)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(1582, 853);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5_exit2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1_exit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3_info)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4_user)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2_options)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3_info;
+        private System.Windows.Forms.PictureBox pictureBox4_user;
+        private System.Windows.Forms.Button Left_Menu_Button;
+        private System.Windows.Forms.PictureBox pictureBox2_options;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1_exit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox5_exit2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
+
---
+++ Regression_manager/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regression_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Left_Menu_Button_Click(object sender, EventArgs e)
+        {
+            if (panel1.Width == 45)
+                panel1.Width = 200;
+            else
+                panel1.Width = 45;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            pictureBox1_Click(sender,e);
+        }
+
+        private void Resize_Menu(dynamic sender, bool bToBIG)
+        {
+            if(bToBIG)
+                sender.Size = new System.Drawing.Size(32, 29);
+            else
+                sender.Size = new System.Drawing.Size(40, 37);
+        }
+
+        #region Animated icons in left menu
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, true);
+        }
+
+        private void pictureBox4_MouseLeave(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, true);
+        }
+
+        private void pictureBox3_MouseLeave(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, true);
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, true);
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox4_MouseMove(object sender, MouseEventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox3_MouseMove(object sender, MouseEventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            pictureBox1_Click(sender, e);
+        }
+
+        private void pictureBox5_MouseMove(object sender, MouseEventArgs e)
+        {
+            Resize_Menu(sender, false);
+        }
+
+        private void pictureBox5_MouseLeave(object sender, EventArgs e)
+        {
+            Resize_Menu(sender, true);
+        }
+
+        #endregion
+
+        #region DragDrop Form without top bar
+        private bool mouseDown;
+        private Point lastLocation;
+
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        #endregion
+
+    }
+}
